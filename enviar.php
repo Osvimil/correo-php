85cc536c530b19c343d4ad51738c4b04e477455e
oswaldo <oswaldoadidas@hotmail.com>
2016-11-07 13:47:58 -0600

    changin php file

--- enviar.php
+++ enviar.php
@@ -1,5 +1,11 @@
 <?php
 
+if(isset($_POST['nombre']) && !empty($_POST['nombre']) &&
+   isset($_POST['correo']) && !empty($_POST['correo'])&&
+   isset($_POST['telefono']) && !empty($_POST['telefono']) &&
+   isset($_POST['mensaje']) && !empty($_POST['mensaje']))
+{
+   
 $destino = 'oswaldoadidas@hotmail.com';
 $nombre = $_POST['nombre'];
 $correo = $_POST['correo'];
@@ -11,4 +17,9 @@ $contenido = "Nombre: " . $nombre. "\nCorreo: " . $correo . "\nTelefono: ". $tel
     
     mail($destino,'Contacto',$contenido);
     header('Location:gracias.html');
+  }else{
+    echo "problemas checa";
+}
+
+
 ?>
